bd166cb5898b85a53a2e82a46200059fedc5d969
Shejal-gaonkar101 <shejalgaonkar2001@gmail.com>
2026-03-25 23:12:28 +0530

    Middle ware practise seesion

--- 03-middleware.js
+++ 03-middleware.js
@@ -5,7 +5,93 @@ function block_1_httpMethods(){
         const app = express()
          app.use(express.json)
          
+        //request Logger //middleware(Built In)
+        //All request must be logged here 
+        //use next when writing middle ware , directs to next middle ware , written at end
+        //this is the basic middle ware 
+        app.use((req,res,next)=>{
+            //Business Logic
+            //add to db
+            //write in some file 
+            
+            const logEntry = `${req.method} :${req.url}`
+            localStorage.push(logEntry)
+            console.log(`[LOG] -- ${logEntry}`)
+
+            //if your request hangs forever 
+            next()
+            
+        })
+
+
+        app.use((req,res,next)=>{
+             req.startTime = Date.now()
+
+             res.on('finish',()=>{
+                const duration = Date.now() - req.startTime
+                console.log(`[Timer] -- ${req.method} - ${req.url} took ${duration}ms`)
+
+             })
+
+             next()
+        })
+
+        function authMe(req,res,next){
+          const token = req.header['x-auth-token']
+
+          if(!token){
+            return res.status(401).json({error: "No Token, Please login"})
+          }
+
+          if(token!=="secret"){
+             return res.status(403).json({error:"Invalid token"})
+           }
+
+           //token -> extract data from token -> userID ,email , admin
+
+           req.user ={id :1,name:"Hitesh",role:"admin"}
+
+           next()
+
+             
+        }
         
+        function getRole(role){
+          return (req,res,next)=>{
+           if(!req.user || req.user.role !== role){
+             return res.status(403).json({error: `Role ${role} required`})
+           }
+        }
+            next()
+        }
+
+        function rateLimit(maxRequest){
+            let count =0
+
+            return (req,res,next)=>{
+                count++
+                if(count>maxRequest){
+                    return res.status(429).json({error:"Too mnay request"}
+                    )
+                }
+                next()
+            }
+        }
+
+        const limitedEndPoint = rateLimit(3)
+        app.get('/limited',limitedEndPoint,(req,res)=>{})
+
+        app.get('/profile',authMe, getRole('admin'), ()=>{})
+        
+
+        app.get('/profile',authMe, getRole('teacher'), ()=>{})
+        
+
+        app.get('/profile',authMe, getRole('student'), ()=>{})
+        
+
+        app.get('/profile',authMe, getRole(['admin','teacher','student']), ()=>{})
+
 
 
 
